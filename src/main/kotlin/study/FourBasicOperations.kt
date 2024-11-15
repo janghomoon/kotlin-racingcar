947fb4715ac7b008f2a4d13c8cfbe970f3b51fba
jhmoon <jhmoon@howser.co.kr>
2024-11-16 05:18:12 +0900

    feat: 사칙연사 구현

---
+++ src/main/kotlin/study/FourBasicOperations.kt
@@ -0,0 +1,46 @@
+package study
+
+class FourBasicOperations {
+    fun operate(
+        beforeValue: Int,
+        targetValue: Int,
+        symbol: String,
+    ): Int {
+        val result: Int =
+            when (symbol) {
+                "+" -> addition(beforeValue, targetValue)
+                "-" -> subtraction(beforeValue, targetValue)
+                "*" -> multiplication(beforeValue, targetValue)
+                else -> division(beforeValue, targetValue)
+            }
+        return result
+    }
+
+    private fun addition(
+        beforeValue: Int,
+        targetValue: Int,
+    ): Int {
+        return beforeValue + targetValue
+    }
+
+    private fun subtraction(
+        beforeValue: Int,
+        targetValue: Int,
+    ): Int {
+        return beforeValue - targetValue
+    }
+
+    private fun multiplication(
+        beforeValue: Int,
+        targetValue: Int,
+    ): Int {
+        return beforeValue * targetValue
+    }
+
+    private fun division(
+        beforeValue: Int,
+        targetValue: Int,
+    ): Int {
+        return beforeValue / targetValue
+    }
+}
